306190e3af3ca6247037d4866f8b1769f042a538
103039 <103039@L150998.centrala.bzwbk>
2016-06-22 17:55:39 +0200

    pocztaki WS

---
+++ eodtWSC/src/eodtwsc/eodtUrlopyClient.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eodtwsc;
+
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.WebParam;
+
+public class eodtUrlopyClient {
+
+    private String urlS;
+    private eodtwsc.EodtUrlopWs_Service service;
+    private eodtwsc.EodtUrlopWs port;//pamietac, aby uzywac getera
+
+    public eodtUrlopyClient() {
+    }
+
+    public eodtUrlopyClient(String urlS) {
+        this.urlS = urlS;
+    }
+
+    public String hello(java.lang.String name) throws MalformedURLException {
+        return this.getPort().hello(name);
+    }
+
+    public String getUrlS() {
+        if (urlS == null) {
+            urlS = "http://127.0.0.1:8080/eodt_j8";
+        }
+        return urlS;
+    }
+
+    public void setUrlS(String urlS) {
+        this.urlS = urlS;
+    }
+
+    private EodtUrlopWs_Service getService() {
+        try {
+            service = new eodtwsc.EodtUrlopWs_Service(new URL(this.getUrlS() + "/EodtUrlopWs?wsdl"));
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(testMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return service;
+    }
+
+    public EodtUrlopWs getPort() {
+        return this.getService().getEodtUrlopWsPort();
+    }
+
+    public String createUrlopCalyDzien(java.lang.String userEmail, java.lang.String login, javax.xml.datatype.XMLGregorianCalendar dataOd,
+            javax.xml.datatype.XMLGregorianCalendar dataDo, java.lang.String rodzajId, String infoDod, boolean pracodawca, BigDecimal kwota,
+            String miejsce, String cel, String srodekLok) {
+        return getPort().createUrlopCalyDzien(userEmail, login, dataOd, dataDo, rodzajId, infoDod, pracodawca, kwota, miejsce, cel, srodekLok);
+    }
+
+    public String createUrlopGodziny(java.lang.String userEmail, java.lang.String login, javax.xml.datatype.XMLGregorianCalendar dataUrlopu, javax.xml.datatype.XMLGregorianCalendar godzOd,
+            javax.xml.datatype.XMLGregorianCalendar godzDo, java.lang.String rodzajId, String infoDod,
+            boolean pracodawca, BigDecimal kwota, String miejsce, String cel, String srodekLok) {
+        return getPort().createUrlopGodziny(userEmail, login, dataUrlopu, godzOd, godzDo, rodzajId, infoDod, pracodawca, kwota, miejsce, cel, srodekLok);
+    }
+
+}
---
+++ eodtWSC/src/eodtwsc/testMain.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eodtwsc;
+
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+/**
+ *
+ * @author 103039
+ */
+public class testMain {
+
+    public static void main(String[] args) throws MalformedURLException, DatatypeConfigurationException {
+
+        //System.err.println(new eodtUrlopyClient("http://localhost:8080/eodt_j8").hello("ssssssssssss"));
+
+        GregorianCalendar c = new GregorianCalendar();
+        c.setTime(new Date());
+        c.add(Calendar.DATE, 4);
+        XMLGregorianCalendar dateOd = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        c.add(Calendar.DATE, 5);
+        XMLGregorianCalendar dateDo = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        Long idWniosku;
+        String wynik=new eodtUrlopyClient("http://localhost:8080/eodt_j8").createUrlopCalyDzien("aa@aa.pl", "kk@kk.pl", dateOd, dateDo, "1", "dddddddd",
+                false, new BigDecimal("111.11"), "miejsce", "cel", "srodekLok");
+        try {//jesli jest ok zwraca nr wniosku, jesli nie pokazuje co bylo nie tak
+            idWniosku=new Long(wynik);
+            System.out.println("id wniosku urlopowego: "+idWniosku);
+        } catch (NumberFormatException nfe){
+            System.err.println(wynik);
+        }
+        
+        c.set(Calendar.HOUR_OF_DAY, 7);
+        XMLGregorianCalendar godzOd = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        c.set(Calendar.HOUR_OF_DAY, 15);
+        XMLGregorianCalendar godzDo = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        wynik=new eodtUrlopyClient("http://localhost:8080/eodt_j8").createUrlopGodziny("aa@aa.pl", "kk@kk.pl", dateOd, godzOd, godzDo, "1","info dod",
+               false, new BigDecimal("111.11"), "miejsce", "cel", "srodekLok");
+        try {//jesli jest ok zwraca nr wniosku, jesli nie pokazuje co bylo nie tak
+            idWniosku=new Long(wynik);
+            System.out.println("id wniosku urlopowego: "+idWniosku);
+        } catch (NumberFormatException nfe){
+            System.err.println(wynik);
+        }
+        
+    }
+
+}
